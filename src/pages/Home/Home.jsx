3b47bfe1823a2b6dbe38b4761e247bbc39091a8f
odayTelbany <oday0595814990@gmail.com>
2023-05-28 08:31:56 +0300

    create the header section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
+import styled from "styled-components"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Wrapper>Home</Wrapper>
   )
 }
+
+
+const Wrapper = styled.section`
+  /* background: ${({theme}) => theme.colors.bg}; */
+  // this code right here is temprory to show home section until we start to work on it
+  /* width:20rem; */
+  /* height:20rem; */
+
+`;
 
 export default Home
