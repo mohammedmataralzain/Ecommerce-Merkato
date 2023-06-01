7fc833355d2fd8347997b67afd2349beea6a94ec
odayTelbany <oday0595814990@gmail.com>
2023-06-01 21:23:05 +0300

    Add the feature section to home page

---
+++ src/components/FeatureProducts/FeatureProducts.jsx
@@ -0,0 +1,31 @@
+import useProductContext from "../../hooks/useProductContext"
+import Product from "../Product/Product";
+import { Wrapper } from "./FeatureProducts.styles";
+
+function FeatureProducts() {
+  const {inLoading, featuredProducts} = useProductContext();
+  console.log(featuredProducts);
+
+  if (inLoading){
+    return <div>Loading...</div>
+  }
+
+
+  return (
+    <Wrapper>
+        <div className="container">
+            <div className="intro-data">Check Now!</div>
+            <div className="common-heading">Our Feature Services</div>
+            <div className="grid grid-three-column">
+                {
+                    featuredProducts.map((product) => {
+                        return <Product key={product.key} {...product}/>
+                    })
+                }
+            </div>
+        </div>
+    </Wrapper>
+  )
+}
+
+export default FeatureProducts
---
+++ src/components/FeatureProducts/FeatureProducts.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    padding: 9rem 0;
+    background-color: ${({theme}) => theme.colors.bg};
+
+    
+`
