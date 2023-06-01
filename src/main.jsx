5efef2c9a9f818bb8967e7b3e733c4c71f41253b
mohammed matar <mtrm47639@gmail.com>
2023-06-01 15:45:53 +0300

    add app provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App.jsx'
 import './index.css'
+import { AppProvider } from './context/productcontext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <AppProvider>
    <BrowserRouter>
     <App />
   </BrowserRouter>
+  </AppProvider>
   </React.StrictMode>,
 )
