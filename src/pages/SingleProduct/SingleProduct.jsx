0a29aa6cc82b422afe8bf9b8861e2cff411fd837
mohammed matar <mtrm47639@gmail.com>
2023-06-03 17:35:55 +0300

    Edit single page with style

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,8 +1,102 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useProductContext from "../../hooks/useProductContext";
+import Loading from "../../components/Loading/Loading";
+import PageNavigation from "../../components/PageNavigation/PageNavigation";
+import FormatPrice from "../../Helpers/FormatPrice";
+import { TbReplace, TbTruckDelivery } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
+import { StyleSingleProduct } from "./styled";
 
+// import {getSingleProductData} from "../../context"
 const SingleProduct = () => {
-  return (
-    <div>SingleProduct</div>
-  )
-}
+  const { getSingleProductData, singleProduct, loadingSingleProduct } =
+    useProductContext();
+  const { id } = useParams();
+  const {
+    id: alias,
+    name,
+    company,
+    price,
+    description,
+    category,
+    stock,
+    stars,
+    reviews,
+    image,
+  } = singleProduct;
 
-export default SingleProduct
+  useEffect(() => {
+    getSingleProductData(id);
+  }, []);
+
+ 
+  
+  return (
+    <StyleSingleProduct>
+      {loadingSingleProduct ? (
+        <div className="loading">
+          <Loading />
+        </div>
+      ) : (
+        <div>
+          <PageNavigation title={name} />
+          <div className="container">
+            <div className="grid grid-two-column">
+              <div className="singleproduct-images"></div>
+              <div className="singleproduct-information">
+                <h2>{name}--{category}</h2>
+                <p>{stars}</p>
+                <p>{reviews}</p>
+                <p className="product-data-price">
+              MRP:
+              <del>
+              <FormatPrice price={price + 250000} />
+              </del>
+            </p>
+            <p className="product-data-price product-data-real-price">
+              Deal of the Day:  <FormatPrice price={price} />
+            </p>
+                <p>{description}</p>
+                <div className="product-data-warranty">
+                  <div className="product-warranty-data">
+                    <TbTruckDelivery className="warranty-icon" />
+                    <p>Free Delivery</p>
+                  </div>
+
+                  <div className="product-warranty-data">
+                    <TbReplace className="warranty-icon" />
+                    <p>30 Days Replacement</p>
+                  </div>
+
+                  <div className="product-warranty-data">
+                    <TbTruckDelivery className="warranty-icon" />
+                    <p>Thapa Delivered </p>
+                  </div>
+
+                  <div className="product-warranty-data">
+                    <MdSecurity className="warranty-icon" />
+                    <p>2 Year Warranty </p>
+                  </div>
+                </div>
+                <p>
+                  Available :{" "}
+                  <span>{stock > 0 ? "In Stock" : "Not Available"}</span>
+                </p>
+                <p>
+                  ID : <span>{id}</span>
+                </p>
+                <p>
+                  Brand : <span>{company}</span>
+                </p>
+                <hr/>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </StyleSingleProduct>
+  );
+};
+
+export default SingleProduct;
---
+++ src/pages/SingleProduct/styled.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const StyleSingleProduct = styled.section`
+  .loading {
+    width: 100%;
+    min-height: 70vh;
+    display: flex;
+    align-items: center;
+  }
+  .grid {
+    padding: 12rem 0;
+  }
+  .product-data-warranty {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 1rem;
+
+    .product-warranty-data {
+      text-align: center;
+
+      .warranty-icon {
+        background-color: rgba(220, 220, 220, 0.5);
+        border-radius: 50%;
+        width: 4rem;
+        height: 4rem;
+        padding: 0.6rem;
+      }
+      p {
+        font-size: 1.4rem;
+        padding-top: 0.4rem;
+      }
+    }
+  }
+  span {
+    color : ${({theme}) => theme.colors.black};
+    font-weight: bold;
+  }
+
+  hr {
+    height: 0.5rem;
+    background-color: ${({theme}) => theme.colors.black};
+    width: calc(100% - 5rem);
+  }
+
+  hr,p {
+    margin: 2rem 0;
+  }
+  .product-data-price {
+      font-weight: bold;
+    }
+    .product-data-real-price {
+      color: ${({ theme }) => theme.colors.btn};
+    }
+`;
