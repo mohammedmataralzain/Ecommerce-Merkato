bb378203637e35bc37ff414b1da07cf339563112
mohammed matar <mtrm47639@gmail.com>
2023-06-07 20:42:56 +0300

    splitting coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 //css.3.3
 
 import "./App.css";
+import React, { lazy } from "react";
 
 //pages
-import Cart from "./pages/Cart/Cart";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
-import SingleProduct from "./pages/SingleProduct/SingleProduct";
-import Error from "./pages/Error/Error";
-import About from "./pages/About/About";
+const Cart = lazy(() => import("./pages/Cart/Cart"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const SingleProduct = lazy(() => import("./pages/SingleProduct/SingleProduct"));
+const Error = lazy(() => import("./pages/Error/Error"));
+const About = lazy(() => import("./pages/About/About"));
 
 //  Routes , Route
 import { Routes, Route } from "react-router-dom";
@@ -17,9 +18,9 @@ import { GlobalStyle } from "../GlobalStyle";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Products from "./pages/Products/Products";
+import Loading from "./components/Loading/Loading";
 
 function App() {
-
   // This will be used as a global style
   const theme = {
     colors: {
@@ -33,30 +34,34 @@ function App() {
       border: "rgba(98, 84, 243, 0.5)",
       hr: "#ffffff",
       helper: "#8490ff",
-      shadow: "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+      shadow:
+        "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
       shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
-      gradient: "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      gradient:
+        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
     },
     media: {
       mobile: "768px",
-      tablet: "998px"
-    }
+      tablet: "998px",
+    },
   };
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/SingleProduct/:id" element={<SingleProduct />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer/>
+      <React.Suspense fallback={Loading()}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/SingleProduct/:id" element={<SingleProduct />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </React.Suspense>
+      <Footer />
     </ThemeProvider>
   );
 }
