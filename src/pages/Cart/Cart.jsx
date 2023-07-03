9778e687ab947a81abca9820763af6defa8f4d43
mohammed matar <mtrm47639@gmail.com>
2023-07-03 21:09:25 +0300

    Add totalCart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,9 +3,10 @@ import CartIteam from "../../components/CartItem/CartIteam";
 import useCartContext from "../../hooks/useCartContext";
 import { Button } from "../../styles/Button";
 import { Wrapper } from "./Cart.styles";
+import FormatPrice from "../../Helpers/FormatPrice";
 
 const Cart = () => {
-  const { cart } = useCartContext();
+  const { shipping_fee, cart, clearCart, total_amount } = useCartContext();
   console.log(cart);
 
   return (
@@ -13,16 +14,11 @@ const Cart = () => {
       <div className="container">
         {cart.length == 0 ? (
           <div className="cart-empty">
-            <div>
-              No items to show, your cart is empty 
-            </div>
+            <div>No items to show, your cart is empty</div>
             <NavLink to="/Products">
-              <Button>
-                add items
-              </Button>
+              <Button>add items</Button>
             </NavLink>
           </div>
-
         ) : (
           <div className="cart-item">
             <div className="cart-heading grid grid-five-column">
@@ -33,9 +29,36 @@ const Cart = () => {
               <p>Remove</p>
             </div>
             <hr />
-            {cart.map((element) => {
-              return <CartIteam key={element.id} {...element} />;
-            })}
+            <div className="cart-items">
+              {cart.map((element) => {
+                return <CartIteam key={element.id} {...element} />;
+              })}
+            </div>
+            <hr />
+
+            <div className="cart-btn">
+              <NavLink to="/Products" className="navbar-link ">
+                <Button>CONTINUE SHOPPING</Button>
+              </NavLink>
+
+              <Button className="clear-btn" onClick={clearCart}>
+                CLEAR CART
+              </Button>
+            </div>
+            <div className="cart-total">
+              <div className="cart-total-info">
+                <p>
+                  Subtotal:
+                  <FormatPrice price={total_amount} />
+                </p>
+                <p>
+                  shipping fee :<FormatPrice price={shipping_fee} />
+                </p>
+                <hr />
+
+                <p>Order Total: <FormatPrice price={total_amount - shipping_fee}/></p>
+              </div>
+            </div>
           </div>
         )}
       </div>
